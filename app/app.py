4e3b7b8cc24b42f5bc988d0109f2be9051adc52f
BrunoPascoa76 <brunopascoa03@ua.pt>
2024-10-16 13:50:50 +0100

    flask config

--- app/app.py
+++ app/app.py
@@ -1,3 +1,12 @@
+import os
 from flask import Flask
 
 app = Flask(__name__)
+
+app.config.update({
+    'AWS_REGION':os.environ['AWS_REGION'],
+    'AWS_COGNITO_USER_POOL_ID':os.environ['AWS_COGNITO_USER_POOL_ID'],
+    'AWS_COGNITO_DOMAIN':os.environ['AWS_COGNITO_DOMAIN'],
+    'AWS_COGNITO_USER_POOL_CLIENT_ID':os.environ['AWS_COGNITO_USER_POOL_CLIENT_ID'],
+    'AWS_COGNITO_REDIRECT_URL':'localhost:5000/redirect'
+})
