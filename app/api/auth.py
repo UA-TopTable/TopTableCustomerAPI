f8b51b997f672a2b91900ac2922d41afde2d7236
BrunoPascoa76 <brunopascoa03@ua.pt>
2024-10-18 12:36:37 +0100

    main auth SHOULD  be working (testing now)

---
+++ app/api/auth.py
@@ -0,0 +1,119 @@
+import os
+import boto3
+from flask import current_app, jsonify, request
+from flask_restx import Namespace,Resource,fields
+from botocore.exceptions import NotAuthorizedException,UsernameExistsException,CodeMismatchException,ExpiredCodeException
+
+api=Namespace("auth",description="Authentication operations")
+
+cognito=boto3.client('cognito-idp',current_app.config["AWS_REGION"])
+
+@api.route("/login")
+class Login(Resource):
+    @api.doc('login')
+    @api.expect({
+    "email":fields.String(required=True),
+    "password":fields.String(required=True)
+    },validate=True)
+    @api.response(200,description="success",model={
+        "AccessToken":fields.String(),
+        "ExpiresIn":fields.Integer(),
+        "TokenType":fields.String(),
+        "RefreshToken":fields.String(),
+        "IdToken":fields.String(),
+        "NewDeviceMetadata":fields.Nested({
+            "DeviceKey":fields.String,
+            "DeviceGroupKey":fields.String
+        })
+    })
+    @api.response(400,"wrong response body")
+    @api.response(401,"wrong email/password")
+    def post(self):
+        try:
+            data=request.json
+            email=data.get("email")
+            password=data.get("password")
+
+            response=cognito.initiate_auth(
+                AuthFlow="USER_PASSWORD_AUTH",
+                AuthParameters={
+                    "USERNAME":email,
+                    "PASSWORD":password
+                },
+                clientId=current_app.config["AWS_USER_POOL_CLIENT_ID"]
+            )
+            return jsonify(response.get("AuthenticationResult"),200)
+        except KeyError:
+            return "Incorrect input",400
+        except NotAuthorizedException:
+            return "Wrong username/password",401
+        
+@api.route("/sign_up")
+class SignUp(Resource):
+    @api.doc("sign up")
+    @api.expect({
+        "email":fields.String(required=True),
+        "password":fields.String(required=True),
+        "name":fields.String(required=True),
+        "phone_number":fields.String(required=True)
+    },validate=True)
+    @api.response(201,"user created")
+    @api.response(400,"wrong body")
+    @api.response(409,"username already exists")
+    def post(self):
+        try:
+            data=request.json
+            email=data.get("email")
+            password=data.get("password")
+            name=data.get("name")
+            phone_number=data.get("phone_number")
+
+            cognito.sign_up(
+                ClientId=current_app.config["AWS_USER_POOL_CLIENT_ID"],
+                Username=email,
+                Password=password,
+                UserAttributes=[
+                    {
+                        "Name": "Name",
+                        "Value":name
+                    },
+                    {
+                        "Name": "Phone Number",
+                        "Value":phone_number
+                    }
+                ]
+            )
+            return "User created. Confirm registration via email"
+        except KeyError:
+            return "Wrong Body",400
+        except UsernameExistsException:
+            return "Username already exists",409
+        
+@api.route("/signup/confirm")
+class ConfirmSignUp(Resource):
+    @api.doc("confirm sign up")
+    @api.expect({
+        "email":fields.String(required=True),
+        "confirmation_code":fields.String(required=True)
+    },validate=True)
+    @api.response(200,"user confirmed")
+    @api.response(400,"wrong body")
+    @api.response(400,"wrong confirmation code")
+    @api.response(410,"confirmation code expired")
+    def post(self):
+        try:
+            data=request.data
+            email=data.get("email")
+            confirmation_code=data.get("confirmation_code")
+            cognito.confirm_sign_up(
+                ClientId=current_app.config["AWS_USER_POOL_CLIENT_ID"],
+                Username=email,
+                ConfirmationCode=confirmation_code
+            )
+            return "user confirmed",200
+        except KeyError:
+            return "Wrong body",400
+        except CodeMismatchException:
+            return "Wrong confirmation code",400
+        except ExpiredCodeException:
+            return "Confirmation code expired",410
--- app/app.py
+++ app/app.py
@@ -1,66 +1,20 @@
 import os
-import secrets
-from flask import Flask,jsonify, redirect, session, url_for
+import boto3
+from flask import Flask,jsonify, redirect, request, session
+from flask_restx import Api
 
-from flask_cognito_lib import CognitoAuth
-from flask_cognito_lib.decorators import (
-    auth_required,
-    cognito_login,
-    cognito_login_callback,
-    cognito_logout,
-    cognito_refresh_callback,
-)
+from api.auth import api as api_auth
 
 app = Flask(__name__)
+api = Api(app,version="1.0",title="TopTable Customer API",description="TopTable API for the customer side")
+api.add_namespace(api_auth)
 
-app.secret_key=secrets.token_urlsafe(16)
-
-
+app.secret_key=os.environ["FLASK_SECRET_KEY"]
 app.config.update({
     "AWS_REGION":os.environ["AWS_REGION"],
     "AWS_COGNITO_USER_POOL_ID":os.environ["AWS_COGNITO_USER_POOL_ID"],
     "AWS_COGNITO_DOMAIN":os.environ["AWS_COGNITO_DOMAIN"],
     "AWS_COGNITO_USER_POOL_CLIENT_ID":os.environ["AWS_COGNITO_USER_POOL_CLIENT_ID"],
-    "AWS_COGNITO_USER_POOL_CLIENT_SECRET":os.environ["AWS_COGNITO_USER_POOL_CLIENT_SECRET"],
-    "AWS_COGNITO_REDIRECT_URL":"http://localhost:5000/redirect",
-    "AWS_COGNITO_LOGOUT_URL":"http://localhost:5000/logout/redirect",
     "AWS_COGNITO_REFRESH_FLOW_ENABLED":True,
     "AWS_COGNITO_REFRESH_COOKIE_ENCRYPTED":True,
 })
-
-auth = CognitoAuth(app)
-
-@app.route("/")
-def home_page():
-    return "Hello, world"
-
-#just here temporarily to showcase need for authentication
-@app.route("/protected")
-@auth_required()
-def get_user_details():
-    return jsonify(session)
-
-@app.route("/login")
-@cognito_login
-def login():
-    pass
-
-@app.route("/redirect")
-@cognito_login_callback
-def redirect():
-    return redirect("/")
-
-@app.route("/refresh", methods=["POST"])
-@cognito_refresh_callback
-def refresh():
-    pass
-
-
-@app.route("/logout")
-@cognito_logout
-def logout():
-    pass
-
-@app.route("/logout/redirect")
-def logout_redirect():
-    return redirect("/")
