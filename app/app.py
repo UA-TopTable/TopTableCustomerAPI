01538cb32431faf0334648e6d9c67188b2ce7387
BrunoPascoa76 <brunopascoa03@ua.pt>
2024-10-18 16:51:12 +0100

    TABLETOP-29 first test passing

--- app/app.py
+++ app/app.py
@@ -5,17 +5,18 @@ from flask_restx import Api
 
 from apis import blueprint,api
 
-app = Flask(__name__)
-api.init_app(app)
+def create_app():
+    app = Flask(__name__)
+    api.init_app(app)
 
-app.secret_key=os.environ["FLASK_SECRET_KEY"]
-app.config.update({
-    "AWS_REGION":os.environ["AWS_REGION"],
-    "AWS_COGNITO_USER_POOL_ID":os.environ["AWS_COGNITO_USER_POOL_ID"],
-    "AWS_COGNITO_DOMAIN":os.environ["AWS_COGNITO_DOMAIN"],
-    "AWS_COGNITO_USER_POOL_CLIENT_ID":os.environ["AWS_COGNITO_USER_POOL_CLIENT_ID"],
-    "AWS_COGNITO_REFRESH_FLOW_ENABLED":True,
-    "AWS_COGNITO_REFRESH_COOKIE_ENCRYPTED":True,
-})
+    app.secret_key=os.environ["FLASK_SECRET_KEY"]
+    app.config.update({
+        "AWS_REGION":os.environ["AWS_REGION"],
+        "AWS_COGNITO_USER_POOL_ID":os.environ["AWS_COGNITO_USER_POOL_ID"],
+        "AWS_COGNITO_DOMAIN":os.environ["AWS_COGNITO_DOMAIN"],
+        "AWS_COGNITO_USER_POOL_CLIENT_ID":os.environ["AWS_COGNITO_USER_POOL_CLIENT_ID"],
+        "AWS_COGNITO_REFRESH_FLOW_ENABLED":True,
+        "AWS_COGNITO_REFRESH_COOKIE_ENCRYPTED":True,
+    })
 
-app.run(debug=True)
+    return app
