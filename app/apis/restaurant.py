2b2323f74b9c2152c8ecb8956ce1c20af228abfd
robinlafage <robinlafage42@gmail.com>
2024-11-14 17:47:35 +0000

    test

--- app/apis/restaurant.py
+++ app/apis/restaurant.py
@@ -27,7 +27,7 @@ class Restaurant(Resource):
             return restaurant.get('id'), 201
         except Exception as e:
             print(e)
-            return f'erreur : {e}', 400
+            return e, 400
 
     @api.doc("Get restaurants") 
     @api.response(200,description="restaurants list")
