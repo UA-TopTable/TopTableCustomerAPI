9809ad18269f4dbe53dc2bc5aa503e3dd5b1fc39
ilkeratik <ilkeratik35@gmail.com>
2024-11-14 20:50:32 +0000

    add root_path_prefix for load balancer

--- app/apis/__init__.py
+++ app/apis/__init__.py
@@ -6,10 +6,15 @@ from .reservation import api as api_reservation
 from .restaurant import api as api_restaurant
 from .dining_table import api as api_table
 from .ui import api as api_ui
+from secret import ROOT_PATH_PREFIX
 
-blueprint = Blueprint('endpoints', __name__)
+blueprint = Blueprint('apis', __name__, url_prefix=ROOT_PATH_PREFIX)
 
-api = Api(version="1.0", title="TopTable Customer API", description="TopTable API for the customer side", prefix="/")
+api = Api(version="1.0",
+          title="TopTable Customer API",
+          description="TopTable API for the customer side",
+          prefix=ROOT_PATH_PREFIX,
+          doc=f'{ROOT_PATH_PREFIX}' )
 
 api.add_namespace(api_auth)
 api.add_namespace(api_reservation)
