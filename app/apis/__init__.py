42dfb959659958a39acf19d6f237961046f0b1d7
ilkeratik <ilkeratik35@gmail.com>
2024-11-15 10:36:38 +0000

    TABLETOP-57 add prefix to swagger assests

--- app/apis/__init__.py
+++ app/apis/__init__.py
@@ -1,5 +1,6 @@
 from flask import Blueprint
 from flask_restx import Api, Namespace
+from flask_restx.apidoc import apidoc
 
 from .auth import api as api_auth
 from .reservation import api as api_reservation
@@ -11,11 +12,13 @@ from secret import ROOT_PATH_PREFIX
 
 blueprint = Blueprint('apis', __name__, url_prefix=ROOT_PATH_PREFIX)
 
+apidoc.static_url_path = f'{ROOT_PATH_PREFIX}/swaggerui'
+
 api = Api(version="1.0",
-          title="TopTable Customer API",
-          description="TopTable API for the customer side",
-          prefix=ROOT_PATH_PREFIX,
-          doc=f'{ROOT_PATH_PREFIX}' )
+        title="TopTable Customer API",
+        description="TopTable API for the customer side",
+        prefix=ROOT_PATH_PREFIX,
+        doc=f'{ROOT_PATH_PREFIX}' )
 
 api.add_namespace(api_auth)
 api.add_namespace(api_reservation)
